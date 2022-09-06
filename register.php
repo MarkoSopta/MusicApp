88a38d2218067d381582a15914a4132a3dc7b52b
MarkoSopta <philoyd99@gmail.com>
2022-09-06 21:48:52 +0200

    fix za volume

--- register.php
+++ register.php
@@ -18,7 +18,7 @@
 <html>
 <head>
 	<title>Music App</title>
-
+	<meta name="viewport" content ="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
